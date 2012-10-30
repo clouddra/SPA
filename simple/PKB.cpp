30aa97995661008915baf8f0eaf071d0bc5d9d34
Shihan Hong <utternoob@hotmail.com>
2012-10-30 18:17:28 +0800

    Wrote PKB api for follows + parent table (Not finished)

--- simple/PKB.cpp
+++ simple/PKB.cpp
@@ -44,6 +44,83 @@ int PKB::insertNode(int nodeType, std::string value, int stmtNum, int parent) {
     return newNode;
 }
 
+int PKB::getParent(int stmt) {
+    return -1;    
+}
+
+std::vector<int> PKB::getParentT(int stmt) {
+    std::vector<int> ans;
+    int curr = stmt;
+    while (curr != -1) {
+        curr = getParent(curr);
+        if (curr != -1)
+            ans.push_back(curr);
+    }
+    return ans;
+}
+
+std::vector<int> PKB::getChild(int stmt) {
+    return std::vector<int> ();
+}
+
+std::vector<int> PKB::getChildT(int stmt) {
+    int curr = 0;
+    std::vector<int> ans = getChild(stmt);
+    
+    while (curr < ans.size()) {
+        std::vector<int> temp = getChild(ans[curr]);
+        for (int i = 0; i < temp.size(); i++) {
+            ans.push_back(temp[i]);
+        }
+        curr++;
+    }
+    return ans;
+}
+
+bool PKB::isParent(int parent, int child) {
+    return false;
+}
+
+std::vector<int> PKB::getFollows(int stmt) {
+    return std::vector<int> ();
+}
+
+std::vector<int> PKB::getFollowsT(int stmt) {
+    int curr = 0;
+    std::vector<int> ans = getFollows(stmt);
+    
+    while (curr < ans.size()) {
+        std::vector<int> temp = getFollows(ans[curr]);
+        for (int i = 0; i < temp.size(); i++) {
+            ans.push_back(temp[i]);
+        }
+        curr++;
+    }
+    return ans;
+}
+
+std::vector<int> PKB::getFollowedBy(int stmt) {
+    return std::vector<int> ();
+}
+
+std::vector<int> PKB::getFollowedByT(int stmt) {
+    int curr = 0;
+    std::vector<int> ans = getFollowedBy(stmt);
+    
+    while (curr < ans.size()) {
+        std::vector<int> temp = getFollowedBy(ans[curr]);
+        for (int i = 0; i < temp.size(); i++) {
+            ans.push_back(temp[i]);
+        }
+        curr++;
+    }
+    return ans;
+}
+
+bool isFollow(int first, int second) {
+    return false;
+}
+
 std::vector<int> PKB::getModifiesVar(std::string var) {
     int varIndex = varTable.getVarIndex(var);
     return modifiesTable.getModifiesVar(varIndex);
--- simple/PKB.h
+++ simple/PKB.h
@@ -41,6 +41,16 @@ private:
 public:
     PKB();
     int insertNode(int nodeType, std::string value, int stmtNum, int parent);
+    int getParent(int stmt);
+    std::vector<int> getParentT(int stmt);
+	std::vector<int> getChild(int stmt);
+    std::vector<int> getChildT(int stmt);
+    bool isParent(int parent, int child);
+    std::vector<int> getFollows(int stmt);
+    std::vector<int> getFollowsT(int stmt);
+	std::vector<int> getFollowedBy(int stmt);
+    std::vector<int> getFollowedByT(int stmt);
+    bool isFollow(int first, int second);
     std::vector<int> getModifiesVar(std::string var);
 	std::vector<std::string> getModifiedBy(int stmt);
     bool isModifies(int stmt, std::string var);
