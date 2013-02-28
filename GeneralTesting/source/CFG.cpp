d86ef27c2f499c46d9bcc3b6bf5cb0962b8c2b80
Yun Long Chong <chong.yunlong@gmail.com>
2013-02-28 18:26:57 +0800

    non recursive next* in CFG.cpp.
    non-recursion because i hate recursion

--- GeneralTesting/source/CFG.cpp
+++ GeneralTesting/source/CFG.cpp
@@ -1,16 +1,17 @@
 #include "CFG.h"
+#include <queue>
 
 std::vector<int> CFG::getNext(int stmt1, int nodeIndex){
 	std::vector<int> stmtList;
-	
+
 	// if fall in range
 	if (stmt1>=cfg[nodeIndex].getStart() && stmt1 < cfg[nodeIndex].getEnd()) {
 		stmtList.push_back(stmt1+1);
 		return stmtList;
 	}
-	
+
 	else {
-		for (int i=0, nextNode; i<cfg[nodeIndex].getNext().size(); i++) {
+		for (int i=0, nextNode; i<(int)cfg[nodeIndex].getNext().size(); i++) {
 			nextNode = cfg[nodeIndex].getNext().at(i);
 			stmtList.push_back(cfg[nextNode].getStart());
 		}
@@ -20,17 +21,65 @@ std::vector<int> CFG::getNext(int stmt1, int nodeIndex){
 
 }
 
+// non-recursive cause i hate recursion
+std::vector<int> CFG::getNextT(int stmt1, int nodeIndex){
+	std::vector<int> stmtList;
+	std::vector<bool> visited(cfg.size(), false);	//initialising cfg
+	std::queue<int> nodesToVisit;
+	int nextNode;
+
+
+	// push statements of current node
+	for (int i=stmt1+1; i<=cfg[nodeIndex].getEnd(); i++)
+		stmtList.push_back(i);
+
+	visited[nodeIndex] = true;
+	// push next nodes to nodesToVisit
+	for (int i=0; i<(int)cfg[nodeIndex].getNext().size(); i++) {
+		nextNode = cfg[nodeIndex].getNext().at(i);
+		nodesToVisit.push(nextNode);
+	}
+
+	while (!nodesToVisit.empty())
+	{
+		nextNode = nodesToVisit.front() ;
+		nodesToVisit.pop();
+		if (visited[nextNode] == false)
+		{
+			visited[nextNode]=true;
+			// add statements to list
+			stmtList = fillStmtInNode(stmtList, cfg[nextNode]);
+
+			// get next nodes to visit
+			for (int i=0; i<(int)cfg[nodeIndex].getNext().size(); i++) {
+				nextNode = cfg[nodeIndex].getNext().at(i);
+				nodesToVisit.push(nextNode);
+			}
+		}
+	}
+
+	return stmtList;
+}
+
+
+std::vector<int> CFG::fillStmtInNode(std::vector<int> stmtList, CFGNode nextNode){
+	for (int i=nextNode.getStart(); i<=nextNode.getEnd(); i++)
+		stmtList.push_back(i);
+
+	return stmtList;
+}
+
 std::vector<int> CFG::getPrev(int stmt2, int nodeIndex){
 	std::vector<int> stmtList;
-	
+
 	// if fall in range
 	if (stmt2 > cfg[nodeIndex].getStart() && stmt2 <= cfg[nodeIndex].getEnd()) {
 		stmtList.push_back(stmt2 - 1);
 		return stmtList;
 	}
-	
+
 	else {
-		for (int i=0, nextNode; i<cfg[nodeIndex].getNext().size(); i++) {
+		for (int i=0, nextNode; i<(int)cfg[nodeIndex].getNext().size(); i++) {
 			nextNode = cfg[nodeIndex].getPrev().at(i);
 			stmtList.push_back(cfg[nextNode].getEnd());
 		}
@@ -53,9 +102,9 @@ int CFG::insertCFGNode(int start, int end, std::vector<int> prev){
 	CFGNode temp = CFGNode(start, end, prev);
 	cfg.push_back(temp);
 	int newNode = cfg.size() - 1;
-	
+
 	// find all previous nodes and add index of new node to their next
-	for (int i=0; i<prev.size(); i++)
+	for (int i=0; i<(int)prev.size(); i++)
 		cfg[prev[i]].addNext(newNode);
 
 	return newNode;
