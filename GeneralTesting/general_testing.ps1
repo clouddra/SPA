ba97f3005eea66c821b7fc7bf8469abbbc47a413
Wei Chee Lim <weichee_kenshix@hotmail.com>
2013-04-18 00:32:50 +0800

    autotester

--- GeneralTesting/general_testing.ps1
+++ GeneralTesting/general_testing.ps1
@@ -140,10 +140,10 @@ write-output $p;
 #"autoTester32", "autoTester34" -> can't run. dunno why.
 
 # uncomment the line below and comment the next line dfor complete tests
-#$all_testing = @("cppunit", "integration", "autoTester", "autoTester1", "autoTester2", "autoTester3", "autoTester4", "autoTester5", "autoTester6", "autoTester7", "autoTester8", "autoTester9", "autoTester10", "autoTester14", "autoTester15", "autoTester16", "autoTester17", "autoTester18", "autoTester19", "autoTester20", "autoTester21", "autoTester22", "autoTester23", "autoTester24", "autoTester25", "autoTester26", "autoTester27", "autoTester28", "autoTester29", "autoTester30", "autoTester31", "autoTester32", "autoTester33", "autoTester34", "autoTester35")
+$all_testing = @("cppunit", "integration", "autoTester", "autoTester1", "autoTester2", "autoTester3", "autoTester4", "autoTester5", "autoTester6", "autoTester7", "autoTester8", "autoTester9", "autoTester10", "autoTester14", "autoTester15", "autoTester16", "autoTester17", "autoTester18", "autoTester19", "autoTester20", "autoTester21", "autoTester22", "autoTester23", "autoTester24", "autoTester25", "autoTester26", "autoTester27", "autoTester28", "autoTester29", "autoTester30", "autoTester31", "autoTester32", "autoTester33", "autoTester34", "autoTester35")
 #$all_testing = @("cppunit", "integration", "autoTester", "autoTester1", "autoTester2", "autoTester3", "autoTester4", "autoTester5", "autoTester6", "autoTester7", "autoTester8", "autoTester9", "autoTester10", "autoTester14", "autoTester15", "autoTester16", "autoTester17", "autoTester18", "autoTester19", "autoTester20", "autoTester21", "autoTester22", "autoTester23", "autoTester24", "autoTester25", "autoTester26", "autoTester27", "autoTester28", "autoTester29", "autoTester31", "autoTester33", "autoTester35")
 #$all_testing = @("autoTester31", "autoTester33");
-$all_testing = @("autoTester31");
+#all_testing = @("autoTester31");
 
 foreach ($testing in $all_testing)
 {
